d2c0db56f3d63777a4772ee9e13bcaccb2fa5f2c
Natalie Abreu <84210490+natalie313@users.noreply.github.com>
2025-09-21 22:06:17 -0300

    Delete 02. Códigos/back/model directory

--- 02. Códigos/back/model/Automovel.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Automovel {
-    private int id;
-    private int ano;
-    private String marca;
-    private String modelo;
-    private String placa;
-}
--- 02. Códigos/back/model/Empresa.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Empresa {
-    private int id;
-    private String nome;
-
-    public void criarEmpresa(){}
-}
