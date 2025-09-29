dd180c696e1d246f426207348c20b1b8309d8b0f
Natalie Abreu <84210490+natalie313@users.noreply.github.com>
2025-09-28 21:32:37 -0300

    Delete 02. Códigos/back directory

--- 02. Códigos/back/model/Agente.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class Agente {
-    public void avaliarPedido(){}
-    public void modificarPedido(){}
-}
--- 02. Códigos/back/model/Relatorio.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import java.util.List;
-
-public class Relatorio {
-    private List<Pedido> Pedidos; 
-}
